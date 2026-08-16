e563be5326ac88cadaf2f5249212fcfa5b93e85f
Mahdi <mahdiazimi.885@gmail.com>
2026-08-17 01:34:10 +0330

    feat : add HibernateUtil

---
+++ src/main/java/ir/maktabsharif/util/HibernateUtil.java
@@ -0,0 +1,41 @@
+package ir.maktabsharif.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.function.Function;
+
+public class HibernateUtil {
+    private static final String PERSISTENCE_UNITE = "digital_library";
+
+    private static EntityManagerFactory emf;
+
+    private HibernateUtil(){
+
+    }
+
+    private static EntityManagerFactory getEmf(){
+        if(emf== null){
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNITE);
+        }
+        return emf;
+    }
+
+    public static <T> T inTxResult(Function<EntityManager, T> operation) {
+        EntityManager em = getEmf().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            T result = operation.apply(em);
+            tx.commit();
+            return result;
+        } catch (RuntimeException e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+}
