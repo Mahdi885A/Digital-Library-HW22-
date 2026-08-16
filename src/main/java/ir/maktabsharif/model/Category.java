8189854dd8693ae471d2bd1585f798d9734fee9b
Mahdi <mahdiazimi.885@gmail.com>
2026-08-17 01:16:51 +0330

    feat : add model Category

---
+++ src/main/java/ir/maktabsharif/model/Category.java
@@ -0,0 +1,55 @@
+package ir.maktabsharif.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL)
+    private List<Book> book;
+
+    public Category(String name, List<Book> book) {
+        this.name = name;
+        this.book = book;
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(List<Book> book) {
+        this.book = book;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", book=" + book +
+                '}';
+    }
+}
